04d9fde6ec465eb08edc941291d980950e3731ce
Jeff Simonitto <jsimonitto@gmail.com>
2020-09-11 14:04:40 -0400

    Examples of configuring output formatting

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,7 +18,22 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            
+            var builder = services.AddControllers();
+
+            // when this is uncommented serialization of json payload
+            // property names will no longer be camelcased they will
+            // maintain their capitalization
+            //builder.AddJsonOptions(o =>
+            //{
+            //    o.JsonSerializerOptions.PropertyNamingPolicy = null;
+            //});
+
+            // Add support for XML output when Accept header requests it
+            builder.AddMvcOptions(o => 
+            {
+                o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
