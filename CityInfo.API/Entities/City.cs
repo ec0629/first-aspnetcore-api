be7097a7efd759853ce1d5d619bd3cc6fb4f7b4d
Jeff Simonitto <jsimonitto@gmail.com>
2020-09-12 22:09:20 -0400

    Defining Entity classes

---
+++ CityInfo.API/Entities/City.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Entities
+{
+    public class City
+    {
+        // Convention automatically recognizes that a field named "Id"
+        // is the primary key. However, we are explicitly marking it as well
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(200)]
+        public string Description { get; set; }
+        public ICollection<PointOfInterest> PointsOfInterest { get; set; }
+            = new List<PointOfInterest>();
+    }
+}
---
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        // By convention a complex type like City is recognized by
+        // the framework as a Navigation Property, and the framework
+        // will define the relationship automatically and will
+        // automatically target the primary key of the parent class
+        [ForeignKey("CityId")]
+        public City City { get; set; }
+        // here we have defined the foreign key property explicitly even
+        // though not required
+        public int CityId { get; set; }
+    }
+}
